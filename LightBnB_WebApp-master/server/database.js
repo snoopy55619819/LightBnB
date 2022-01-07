4ca6bbc5cd6c85d40d4eb5c9656562da6f033b94
snoopy55619819 <nikhiltallapureddy@gmail.com>
2022-01-07 04:13:51 +0000

    Update getAllReservations query to use node-postgres

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -70,7 +70,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`
+    SELECT * FROM reservations
+    WHERE guest_id = $1
+    LIMIT $2
+    `, [guest_id, limit])
+    .then((result) => {
+      console.log(result.rows)
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getAllReservations = getAllReservations;
 
